b6c7e291a3c8d576a8598b78adb534b4620da6e0
Anand <anandtrex@users.noreply.github.com>
2019-02-08 11:30:07 +0100

    Adding some plotting functions

--- arm_2link_trajectory.py
+++ arm_2link_trajectory.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.interpolate import interp1d
 
 
 def simulate(arm, time_span, dt, control, q0=None, dq0=None):
@@ -18,33 +19,71 @@ def simulate(arm, time_span, dt, control, q0=None, dq0=None):
     return q[1:], dq[1:]
 
 
-def _check_simulate():
+def generate_follow_input(Tmax, dt):
+    Tperiod = 1.
+    inputreduction = 0.3
+    reprRadius = 1.0
+    N = 4
+
+    heights = np.random.normal(size=(N // 2, int(Tmax / Tperiod) + 1))
+    heights = heights / np.linalg.norm(heights, axis=0) / inputreduction
+    ## random uniform 'white-noise' with 50 ms steps interpolated
+    ##  50ms is longer than spiking-network response-time, and assumed shorter than tau-s of the dynamical system.
+    noisedt = 50e-3
+    # cubic interpolation for long sim time takes up ~64GB RAM and then hangs, so linear or nearest interpolation.
+    noiseN = np.random.uniform(-reprRadius / inputreduction, reprRadius / inputreduction,
+                               size=(N // 2, int(Tmax / noisedt) + 1))
+    noisefunc = interp1d(np.linspace(0, Tmax, int(Tmax / noisedt) + 1), noiseN, kind='linear', \
+                         bounds_error=False, fill_value=0., axis=1)
+    heightsfunc = interp1d(np.linspace(0, Tmax, int(Tmax / Tperiod) + 1), heights, kind='linear', \
+                           bounds_error=False, fill_value=0., axis=1)
+
+    #inpfn = lambda t: noisefunc(t) + heights[:,int(t/Tperiod)]*reprRadius
+    inpfn = lambda t: noisefunc(t) + heightsfunc(t) * reprRadius
+    ts = np.arange(0, Tmax, dt)
+
+    return inpfn(ts)
+
+
+def generate_gp_input(Tmax, dt, arm):
     import george
-    from arm_2link_todorov_gravity import Arm
-    arm = Arm()
-    q0, dq0 = (np.zeros(arm.dim_config_space) for _ in range(2))
-    simulation_time_span = 10.
-    dt = .001
-    t = np.arange(0, simulation_time_span, dt)
+    # q0, dq0 = (np.zeros(arm.dim_config_space) for _ in range(2))
+    t = np.arange(0, Tmax, dt)
     kernel = george.kernels.ExpSquaredKernel(.5)
     np.random.seed(3000)
     gp = george.GP(kernel)
     u = gp.sample(t[::10], arm.dim_config_space) * 4.
     u = np.array([np.interp(t, t[::10], u[i]) for i in range(u.shape[0])])
+    return u
+
+
+def _check_simulate():
+    from arm_2link_todorov_gravity import Arm
+    arm = Arm()
+    q0, dq0 = (np.zeros(arm.dim_config_space) for _ in range(2))
+
+    simulation_time_span = 10.  # s
+    dt = .001  # s
+
+    u = generate_gp_input(simulation_time_span, dt, arm)
+    # u = generate_follow_input(simulation_time_span, dt)
     # time major
     u = u.T
 
     import matplotlib.pyplot as plt
     n_arms = 7
-    lengths = np.linspace(.3, 1.6, n_arms)
+    # lengths = np.linspace(.3, 1.6, n_arms)
     fig, axes = plt.subplots(n_arms + 1, figsize=(16, 10), sharex=True)
     axes[0].plot(u)
     axes[0].set_ylabel('control torques')
     for i in range(n_arms):
-        arm.l1 = lengths[i]
+        arm.l1 = np.random.uniform(0.5, 3.)
+        arm.l2 = np.random.uniform(0.5, 3.)
+        arm.m1 = np.random.uniform(0.5, 3.)
+        arm.m2 = np.random.uniform(0.5, 3.)
         rq, drq = simulate(arm, simulation_time_span, dt, u, q0, dq0)
         axes[i + 1].plot(rq)
-        axes[i + 1].set_ylabel('arm configuration $l_1 = {}$'.format(arm.l1))
+        axes[i + 1].set_ylabel('$l_1 = {:.2f}$\n$l_2 = {:.2f}$\n$m_1 = {:.2f}$\n$m_2 = {:.2f}$'.format(arm.l1, arm.l2, arm.m1, arm.m2))
     fig.tight_layout()
     plt.show()
 
